845c65d542c128246d0e3dfb59e76c69b38a7bc3
guolyxd <guolyxd@163.com>
2023-01-13 21:27:54 +0800

    remove unused import in logoutController

--- blog_parent/src/main/java/com/mszlu/blog/controller/LogoutController.java
+++ blog_parent/src/main/java/com/mszlu/blog/controller/LogoutController.java
@@ -2,15 +2,12 @@ package com.mszlu.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mszlu.blog.service.LoginService;
 import com.mszlu.blog.vo.Result;
-import com.mszlu.blog.vo.params.LoginPara;
 
 @RestController
 @RequestMapping("logout")
